33441d00131bb3a0a2c422b2c377b20a58214e09
joelsct98 <joelsct98@gmail.com>
2020-12-29 14:51:29 +0100

    haciendo los comentarios

--- JS/HeredarArticulos.js
+++ JS/HeredarArticulos.js
@@ -9,6 +9,28 @@ for (var i=0; i < vars.length; i++) {
 
 var ArticleId= pair
 
+/*-----------------botones comentarios------------------*/
+function Responder() {
+    var resul = document.getElementById('resultado_text');
+
+    resul.setAttribute("style","display:block;");
+    //resul.style.display = "block";
+    //var ArticleId=boton.value;
+    //window.location=link+"?Id="+ArticleId;
+    return resul;
+};
+
+function Cerrar() {
+    var resul = document.getElementById('resultado_text');
+
+    resul.setAttribute("style","display:none;background-color: black; color: white;");
+    //resul.style.display = "block";
+    //var ArticleId=boton.value;
+    //window.location=link+"?Id="+ArticleId;
+    return resul;
+};
+/*-----------------botones comentarios------------------*/
+
 
 fetch('http://localhost:8080/Bayiva/api/articles/getByArticlesId/'+pair[1], {
     method: 'GET',
@@ -44,8 +66,6 @@ fetch('http://localhost:8080/Bayiva/api/articles/getByArticlesId/'+pair[1], {
     console.log("----arrayCajita----");
 
 */
-
-
 });
 
 
@@ -68,12 +88,69 @@ fetch('http://localhost:8080/Bayiva/api/comentariosA/getByArticleId/'+pair[1], {
     //console.log(arr);
 
     var datos = JSON.parse(arr);
-    console.log(datos);
+    //console.log(datos);
+
+    var cajita = [];
+    for(let item of datos){
+        /*
+        -Lo puedo usar para coger solo un valor
+        -del objeto en cuestion
+
+        cajita.push(item.texto);
+        console.log(item.texto);
+        */
+        cajita.push(item);
+        //console.log(item);
+    }
+
+    console.log("----array----");
+    for(var i= 0; i<=cajita.length-1; i++){
+
+        console.log(cajita[i]);
+    }
+    console.log("----array----");
+
+    var container = document.getElementById("PosicionarComentarios");
+    for(var i= 0; i<=cajita.length-1; i++){
+        var divContenedor = document.createElement("div");
+        divContenedor.setAttribute("class","mt-3 mb-2");
+        divContenedor.setAttribute("id","comentarios"+i);
+
+        divContenedor.innerHTML+=<h3></h3>
+
+        //contenedor del contenido
+
+        container.appendChild(divContenedor);
+
+    }
+
+    for(var i= 0; i<=cajita.length-1; i++){
+        var divContenedor = document.createElement("div");
+        divContenedor.setAttribute("class","mt-3 mb-2");
+        divContenedor.setAttribute("id","comentarios"+i);
+
+        divContenedor.innerHTML+=cajita[i].imagesAS[0].url;
+
+        //contenedor del contenido
+
+        container.appendChild(divContenedor);
+
+    }
+
+
 
 
 
 
 });
+
+
+
+
+
+
+
+
 
 
 
